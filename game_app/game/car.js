4a253237fea087c7db4bd1e05d50102308747a4e
StuartJSquires <SSquires@360incentives.com>
2017-03-16 14:44:10 -0400

    Heavy refactoring, but with broken movement. Should have split this up into smaller commits.

---
+++ game_app/game/car.js
@@ -0,0 +1,140 @@
+/*
+    The train car class
+
+    the train car can be attached to other cars to form a chain
+
+    TODO put all the values into the config file
+
+    TODO make the cars not decouple from the train
+
+    car types:
+        0- misc car
+        1- engine
+*/
+
+var headingsToVector = {
+  // TODO put this in some sort of Utils file?
+  0: [0, -1], // north
+  1: [1, 0], // east
+  2: [0, 1], // south
+  3: [-1, 0] // west
+}
+
+// class methods
+function tick () {
+  // base cars don't do anything but others might
+  // this is not where movement is resolved this is for shooting and stuff
+
+  if (this.attached_back) {
+    this.attached_back.tick()
+  }
+}
+
+function snapshot () {
+  return {
+    x: this.x * 100,
+    y: this.y * 100,
+    angle: this.angle,
+    type: this.type
+  }
+}
+
+function getTurn () {
+    // get the cars action
+  if (!this.attached_front) return 1 // go straight
+
+  if (this.speed < 0) {
+    // follow the car attached to the back of this car
+    if (!this.attached_back) {
+      return 1
+    } else {
+      // go towards the car you're attached to by following their heading
+      if (this.attached_back.heading === this.heading) {
+        return 1
+      } else {
+        // todo verify this since I made it up with a gut feeling and it's likely wrong -a
+        return ((3 + this.heading - this.attached_back.heading) % 4)
+      }
+    }
+  }
+
+  // return this.attached_front.last_turn
+  return (3 + this.heading - this.attached_front.heading) % 4
+}
+
+function updatePosition () {
+  // update the car's position
+
+  // update the car's velocity
+  if (!this.attached_front) {
+    // todo make unattached cars behave well
+  } else {
+    this.speed = this.attached_front.speed
+  }
+
+  if (this.speed < 0 && !this.reversing) {
+    this.reversing = 1
+    this.progress = 0.99 - this.progress
+    if (this.midturn !== 0) {
+      // resolve the incomplete turn
+      this.heading = (this.heading + this.midturn + 4) % 4// important to turn the train!
+      this.midturn = 0
+    }
+  }
+
+  if (this.speed > 0 && this.reversing) {
+    this.reversing = 0
+    this.progress = 0.99 - this.progress
+    if (this.midturn !== 0) {
+      // resolve the incomplete turn
+      this.heading = (this.heading + this.midturn + 4) % 4// important to turn the train!
+      this.midturn = 0
+    }
+  }
+
+  this.game.map.move(this)
+
+  this.turning = this.getTurn()// this has to be done after moving but also in proper order
+    // call the next car
+  if (this.attached_back) {
+    this.attached_back.updatePosition()
+  }
+}
+
+function attach (target) {
+    // attach the car to another car
+  this.attached_front = target
+  this.game = target.game
+
+  target.attached_back = this
+
+  this.heading = target.last_heading
+  this.last_heading = target.last_heading// MAJOR TODO, if the track isn't straight it will cause an error
+  this.progress = target.progress
+    // console.log(target);
+  this.x = target.x - headingsToVector[target.heading][0]
+  this.y = target.y - headingsToVector[target.heading][1]
+
+  this.player = target.player
+
+  this.player.cars.push(this)
+}
+
+function carFactory () {
+  return {
+    angle: 0,
+    type: 0,
+    turning: 1,
+    midturn: 0,
+    progress: 0,
+    speed: 0,
+    reversing: 0,
+    tick,
+    snapshot,
+    getTurn,
+    updatePosition,
+    attach
+  }
+}
+
+module.exports = carFactory
---
+++ game_app/game/gun-car.js
@@ -0,0 +1,58 @@
+/*
+    This class inherits from the car class
+    The gun car can shoot projectiles
+
+*/
+
+const path = require('path')
+const _ = require('lodash')
+
+const carFactory = require(path.resolve(__dirname, 'car.js'))
+const Projectile = require(path.resolve(__dirname, 'projectile.js'))
+
+function tick () {
+  if (this.reload) {
+    this.reload -= 1
+  } else if (this.player.actions.clicking) {
+    this.shoot()
+  }
+
+  if (this.attached_back) {
+    this.attached_back.tick()
+  }
+}
+
+function shoot () {
+  if (this.reload === 0) {
+        // console.log("pew pew!")
+    const args = {
+      x: this.x,
+      y: this.y,
+      speed: 20,
+      range: 20,
+      player: this.player
+    }
+    const dir = {x: 0, y: 0}
+        // todo this formula is wrong
+    console.log(this.player.mouse.x, this.x)
+    dir.x = (this.player.mouse.x - this.x) / (Math.abs(this.player.mouse.x - this.x) + Math.abs(this.player.mouse.y - this.y))
+    dir.y = (this.player.mouse.y - this.y) / (Math.abs(this.player.mouse.x - this.x) + Math.abs(this.player.mouse.y - this.y))
+    args.dir = dir
+    console.log(dir)
+    const test = new Projectile(args)
+    this.reload = 30
+  }
+}
+
+function gunCarFactory () {
+  return _.merge({}, carFactory(), {
+    type: 2,
+    hp: 100,
+    reload: 0,
+    tick,
+    shoot
+  })
+}
+
+// export our class
+module.exports = gunCarFactory
